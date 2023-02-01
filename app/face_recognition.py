114867756c21c2870f417f4be911751f288ee741
jose hernandez <hernandez240@hotmail.com>
2023-02-01 16:52:57 +0100

    first commit2

--- app/face_recognition.py
+++ app/face_recognition.py
@@ -2,11 +2,17 @@ import numpy as np
 import sklearn
 import pickle
 import cv2
+import os
 
+MODEL_DIR= 'model'
+svm_path=os.path.join(MODEL_DIR,"model_svm.pickle")
+#x=open(svm_path,mode='rb')
+x=open('./model/haarcascade_frontalface_default.xml','rb')
 
 # Load all models
 haar = cv2.CascadeClassifier('./model/haarcascade_frontalface_default.xml') # cascade classifier
-model_svm =  pickle.load(open('./model/model_svm.pickle',mode='rb')) # machine learning model (SVM)
+#model_svm =  pickle.load(open('./model/model_svm.pickle',mode='rb')) # machine learning model (SVM)
+model_svm =  pickle.load(x)
 pca_models = pickle.load(open('./model/pca_dict.pickle',mode='rb')) # pca dictionary
 model_pca = pca_models['pca'] # PCA model
 mean_face_arr = pca_models['mean_face'] # Mean Face
